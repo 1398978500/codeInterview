8008478e1cfab73e22b2aced7b9b4932066745ae
longwei <1398978500@qq.com>
2022-09-24 18:53:17 +0800

    c格式化

--- Test/threadPool.cpp
+++ Test/threadPool.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <deque>
 #include <unistd.h>
+#include <deque>
+#include <iostream>
 
 using namespace std;
 
-static inline void ERR_EXIT_THREAD(int err, const char* msg) {
+static inline void ERR_EXIT_THREAD(int err, const char* msg)
+{
     fprintf(stderr, "%s:%s", strerror(err), msg);
     exit(EXIT_FAILURE);
 }
@@ -18,28 +19,27 @@ class ThreadPool;
 // 任务队列
 struct TaskEle {
 public:
-    void (*taskCallBack)(void* arg); // 回调函数
-    string user_data; // 函数参数
+    void (*taskCallBack)(void* arg);  // 回调函数
+    string user_data;                 // 函数参数
 
-    void setFunc(void (*tcb)(void* arg)) {
+    void setFunc(void (*tcb)(void* arg))
+    {
         taskCallBack = tcb;
     }
 };
 
 // 执行队列节点数据结构
-struct ExecEle
-{
+struct ExecEle {
 public:
-    pthread_t tid; // 线程id
-    bool usable = true; // 当前线程是否可用
-    ThreadPool* pool; // 指向中枢管理的指针
+    pthread_t tid;       // 线程id
+    bool usable = true;  // 当前线程是否可用
+    ThreadPool* pool;    // 指向中枢管理的指针
 
     static void* start(void* arg);
 };
 
 // 线程中枢管理
-struct ThreadPool
-{
+struct ThreadPool {
 public:
     // 任务队列和执行队列
     deque<TaskEle*> task_queue;
@@ -55,13 +55,15 @@ public:
     int thread_count;
 
     // 构造函数
-    ThreadPool(int thread_count) : thread_count(thread_count) {
+    ThreadPool(int thread_count) : thread_count(thread_count)
+    {
         pthread_cond_init(&cont, NULL);
         pthread_mutex_init(&mutex, NULL);
     }
 
     // 创建线程池
-    void createPool() {
+    void createPool()
+    {
         int ret;
 
         // 初始化执行队列
@@ -73,6 +75,11 @@ public:
                 ERR_EXIT_THREAD(ret, "pthread_create");
             }
 
+            // 线程分离
+            for (int i = 0; i < exec_queue.size(); ++i) {
+                pthread_detach(exec_queue[i]->tid);
+            }
+
             fprintf(stdout, "create thread %d\n", i);
             exec_queue.push_back(ee);
         }
@@ -80,7 +87,8 @@ public:
     }
 
     // 加入任务
-    void push_task(void(*tcb)(void* arg), int i) {
+    void push_task(void (*tcb)(void* arg), int i)
+    {
         TaskEle* te = new TaskEle;
         te->setFunc(tcb);
         te->user_data = "Task " + to_string(i) + " run in thread";
@@ -98,7 +106,8 @@ public:
     }
 
     // 析构
-    ~ThreadPool() {
+    ~ThreadPool()
+    {
         for (int i = 0; i < exec_queue.size(); ++i) {
             exec_queue[i]->usable = false;
         }
@@ -112,11 +121,6 @@ public:
 
         pthread_mutex_unlock(&mutex);
 
-        // 等待线程退出
-        for (int i = 0; i < exec_queue.size(); ++i) {
-            pthread_join(exec_queue[i]->tid, NULL);
-        }
-
         // 清空执行队列
         exec_queue.clear();
 
@@ -126,12 +130,13 @@ public:
     }
 };
 
-void* ExecEle::start(void* arg) {
+void* ExecEle::start(void* arg)
+{
     ExecEle* ee = (ExecEle*)arg;
 
     while (true) {
         pthread_mutex_lock(&(ee->pool->mutex));
-        while (ee->pool->task_queue.empty()) { // 队列为空 等待新任务
+        while (ee->pool->task_queue.empty()) {  // 队列为空 等待新任务
             if (!ee->usable) {
                 break;
             }
@@ -160,21 +165,29 @@ void* ExecEle::start(void* arg) {
 }
 
 // 线程执行的业务函数
-void execFunc(void* arg) {
+void execFunc(void* arg)
+{
     TaskEle* te = (TaskEle*)arg;
     fprintf(stdout, "%s\n", te->user_data.c_str());
 }
 
 int main(int argc, char const* argv[])
 {
-    ThreadPool pool(2);
+    if (argc < 2) {
+        cout << "确少参数" << endl;
+        cout << "格式 " << argv[0] << " 线程数" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int num = atoi(argv[1]);
+    ThreadPool pool(num);
     pool.createPool();
     // 创建任务
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < 10; ++i) {
         pool.push_task(&execFunc, i);
     }
 
-    sleep(2);
+    sleep(1);
 
     return 0;
 }
